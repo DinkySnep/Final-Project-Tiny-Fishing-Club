b2e59a1ff3a7665caf35c006d0fe8bc158beb4dc
Treehatshrimp <jasonjiang000@gmail.com>
2026-05-05 01:25:09 -0500

    Added soumd

--- Assets/_Scripts/_Minigame Logic/CastButton.cs
+++ Assets/_Scripts/_Minigame Logic/CastButton.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CastButton : MonoBehaviour
 {
     [SerializeField] private FishingRod fishingRod;
+    public AudioClip castSound;
 
     public void OnCastPressed()
     {
         fishingRod.Cast();  // player cast a line
+        AudioSource.PlayClipAtPoint(castSound, Camera.main.transform.position);
     }
 }
